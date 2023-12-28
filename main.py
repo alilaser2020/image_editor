8b380fcdd70fe8ae7288962f3c1f6d51f7844e46
ali <alilaser8080@gmail.com>
2023-12-28 23:25:21 +0330

    Add version2: upload image

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ import PySimpleGUI as sg
 from PIL import Image, ImageFilter, ImageOps
 from io import BytesIO
 
+
+def update_image(original, blur, contrast, emboss, contour, flipx, flipy):
+    """
+    A method for updating given image
+    :param original:
+    :param blur:
+    :param contrast:
+    :param emboss:
+    :param contour:
+    :param flipx:
+    :param flipy:
+    :return:
+    """
+    image = original.filter(ImageFilter.GaussianBlur(blur))
+    image = image.filter(ImageFilter.UnsharpMask(contrast))
+    if emboss:
+        image = image.filter(ImageFilter.EMBOSS())
+    if contour:
+        image = image.filter(ImageFilter.CONTOUR())
+    if flipx:
+        image = ImageOps.mirror(image)
+    if flipy:
+        image = ImageOps.flip(image)
+
+    bio = BytesIO()
+    image.save(bio, format="PNG")
+    window["-IMAGE-"].update(data=bio.getvalue())
+
+
 sg.theme("GrayGrayGray")
 
 image_col = sg.Column([[sg.Image("test.png", key="-IMAGE-")]])
@@ -21,11 +50,21 @@ layout = [
 ]
 
 window = sg.Window("Image Editor", layout)
-original = Image.open("test.png")
+image_path = sg.popup_get_file("Open", no_window=True)
+original = Image.open(image_path)
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=50)
     if event == sg.WIN_CLOSED:
         break
+
+    update_image(original,
+                 values["-BLUR-"],
+                 values["-CONTRAST-"],
+                 values["-EMBOSS-"],
+                 values["-CONTOUR-"],
+                 values["-FLIPX-"],
+                 values["-FLIPY-"]
+                 )
 
 window.close()
